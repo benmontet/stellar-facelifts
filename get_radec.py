e1a390fb3f5bb1a8eb2ba77a37f58637b229c50d
megbedell <megbedell@gmail.com>
2019-04-22 14:45:27 -0700

    more complete cross-match

--- get_radec.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-a = np.loadtxt('cumulative.csv', skiprows=58, usecols=[-3, -2, -1], delimiter=',')
-
-np.savetxt('ra_dec.txt', a, fmt='%.5f %.6f %.3f', delimiter=',')
--- k2_radec.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-a = np.loadtxt('k2candidates.csv', skiprows=34, usecols=[11, 13, -2], delimiter=',')
-
-np.savetxt('k2_ra_dec.txt', a, fmt='%.5f %.6f %.3f', delimiter=',')
